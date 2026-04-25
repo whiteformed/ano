01d058a3e2576a8e42c490d91517b3437ce25f45
whiteformed <whiteformed@gmail.com>
2026-04-25 10:51:50 +0300

    fx

--- src/pages/WelcomePage/WelcomePage.svelte
+++ src/pages/WelcomePage/WelcomePage.svelte
@@ -7,7 +7,12 @@
 
 <div class="bg-background h-screen w-screen flex justify-center items-center">
 	<BlurFade delay={0.2} offset={40} duration={0.4} class="h-fit w-fit flex flex-col items-center">
-		<HyperText delay={100} text="СВЕТПОЗНАНИЯ." animateOnHover={false} class="w-fit text-4xl md:text-6xl" />
+		<HyperText
+			delay={100}
+			text="СВЕТПОЗНАНИЯ."
+			animateOnHover={false}
+			class="w-fit text-4xl md:text-6xl"
+		/>
 		<DitherShader src={Lotus} class="h-40 w-40" />
 	</BlurFade>
 </div>
